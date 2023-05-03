d0db64ba0a9ae2719eec813a3974a8a1762d6a94
kypkk <kypss50102@gmail.com>
2023-05-03 11:54:28 +0800

    feat: qrcode app

--- App.tsx
+++ App.tsx
@@ -1,10 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useRef, useState } from "react";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import QRCode from "react-native-qrcode-svg";
 
 export default function App() {
+  const [url, setUrl] = useState<string>("");
+  const [qrcode_url, setQrcodeURL] = useState<string>("");
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      {qrcode_url && (
+        <QRCode
+          value={qrcode_url}
+          size={150}
+          color="black"
+          backgroundColor="white"
+        />
+      )}
+      <TextInput
+        style={{
+          height: 40,
+          width: 200,
+          padding: 5,
+          marginTop: 10,
+          borderWidth: 1,
+          borderRadius: 20,
+        }}
+        value={url}
+        onChangeText={setUrl}
+        placeholder="Enter url here 😁"
+      />
+      <Button
+        title="Generate Qrcode"
+        onPress={(e) => {
+          e.preventDefault();
+          setQrcodeURL(url);
+          setUrl("");
+        }}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +46,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
